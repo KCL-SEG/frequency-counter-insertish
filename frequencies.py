92d1255436d9ba12fc04be58e4a19a1f0de16599
Paul Makles <paulmakles@gmail.com>
2022-09-24 19:33:44 +0100

    feat: add solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for item in items:
+        key = str(item)
+        if not key in frequencies:
+            frequencies[key] = 0
+
+        frequencies[key] += 1
+
     return frequencies
